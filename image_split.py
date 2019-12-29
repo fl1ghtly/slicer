6245215ae3d193dd9bfaeebfd722766ed498edff
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2019-12-28 21:07:35 -0800

    Added new functions and updated transparent
    Added functions 'store' and 'create_tuple'. 'store' allows the transparent func. to continue running whilst keeping the variables that it came up with. 'create_tuple' forms the necessary tuples from those values that were stored and returns an array of them to be used. The main body of transparent has been changed so when it reaches the endpoint of an image, it will start to crop the images.

--- image_split.py
+++ image_split.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import os
 
+left_arr = up_arr = right_arr = low_arr = []
+
 def create_dir(dirName):
     if not os.path.exists(dirName):
         try:
@@ -10,79 +12,87 @@ def create_dir(dirName):
 
 def crop(cut_arr, image):
     create_dir('./test')
+    img_num = 1
     for cut in cut_arr:
         new_img = image.crop(cut)
-        new_img.save('./test' + '/result' + cut + '.png')
+        new_img.save('./test' + '/result' + img_num + '.png')
+        img_num += 1
+
+def create_tuple(left_arr, up_arr, right_arr, low_arr):
+    cut_arr = list()
+    if left_arr.len() == right_arr.len() == up_arr.len() == low_arr.len():
+        for x in left_arr.len(): 
+            cut_tuple = tuple(left_arr[x], right_arr[x], up_arr[x], low_arr[x])
+            cut_arr.append
+            if x >= left_arr.len():
+                return cut_arr
+    else:
+        pass # raise exception and print error
+
+def store(left, upper, right, lower):
+    if left is not None:
+        left_arr.append(left)
+    if upper is not None:
+        up_arr.append(upper)
+    if left is not None:
+        right_arr.append(right)
+    if left is not None:
+        low_arr.append(lower)
+    
 
 def transparent(image, data):
     # TODO define image class with these variables
     width, height = image.size
-    row_num = 0
-    col_num = 0
-    row_detect = False # Used incase there are more than 1 pix gap for upper bound
-    col_detect = False
-    end_search = False
-    left = 0
-    upper = 0
-    right = 0
-    lower = 0
-    i = 0
-    j = 0
-    cut_pos = [None] * 4
-    cut_arr = []
+    row_num = col_num = i = j = 0
+    left = upper = right = lower = int()
+    row_detect = col_detect = end_search = False 
+    # Used incase there are more than 1 pix gap for upper bound
 
-    #(left, *upper, right, *lower)
-    while i <= width - 1:
-        if row_num <= height - 1:
-            if data[i + (width * row_num)] is not 0:
-                row_num += 1
-                i = 0
-                row_detect = True
-            elif i == width - 1 and row_detect == False:
-                i = 0
-                upper += 1
-                cut_pos[1] = upper
-                row_num += 1
-            elif i == width - 1 and row_detect == True:
-                lower = row_num
-                cut_pos[3] = lower
-                row_num += 1
-                #i = 0
-                break
-            else:
-                i += 1
+    if col_num > width - 1 and row_num > height - 1:
+        print('reached')
+        arr = create_tuple(left_arr, up_arr, 
+                               right_arr, low_arr)
+        crop(arr, image)
+    else:
+        #(left, *upper, right, *lower)
+        while i <= width - 1:
+            if row_num <= height - 1:
+                if data[i + (width * row_num)] is not 0:
+                    row_num += 1
+                    i = 0
+                    row_detect = True
+                elif i == width - 1 and row_detect == False:
+                    i = 0
+                    upper += 1
+                    row_num += 1
+                elif i == width - 1 and row_detect == True:
+                    lower = row_num
+                    row_num += 1
+                    i = 0
+                    store(None, upper, None, lower)
+                    continue
+                else:
+                    i += 1
 
-    #(*left, upper, *right, lower)
-    while j <= height - 1: 
-        if col_num <= width - 1:
-            if data[col_num + (j * width)] is not 0: 
-                col_num += 1
-                j = 0
-                col_detect = True
-            elif j == height - 1 and col_detect == False:
-                j = 0
-                col_num += 1
-                left += 1
-                cut_pos[0] = left
-            elif j == height - 1 and col_detect == True:
-                right = col_num    
-                cut_pos[2] = right
-                col_num += 1
-                #j = 0
-                end_search = True
-                break
-            else:
-                j += 1          
-
-    if end_search: # activate after every loop
-        end_search = False
-        j = 0
-        i = 0
-        cut_tuple = tuple(cut_pos)
-        cut_arr.append(cut_tuple)
-        print(cut_arr)
-        if col_num > width - 1 and row_num > height - 1:
-            crop(cut_arr, image)
+        #(*left, upper, *right, lower)
+        while j <= height - 1: 
+            if col_num <= width - 1:
+                if data[col_num + (j * width)] is not 0: 
+                    col_num += 1
+                    j = 0
+                    col_detect = True
+                elif j == height - 1 and col_detect == False:
+                    j = 0
+                    col_num += 1
+                    left += 1
+                elif j == height - 1 and col_detect == True:
+                    right = col_num    
+                    store(left, None, right, None)
+                    col_num += 1
+                    j = 0
+                    continue
+                else:
+                    j += 1  
 
 image = Image.open('./test-images/test.png')
 pixels = image.convert('RGBA')
