176495b708d89a5f14fd2c0d57398b96af19990c
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2019-12-30 12:51:40 -0800

    function updates and clean up
    Finally made the program work correctly using the test images. Cleaned up various notes that have become irrelevant. Added new notes for later updates

--- image_split.py
+++ image_split.py
@@ -11,26 +11,25 @@ def create_dir(dirName):
             print('Error creating ' + dirName)
 
 def crop(cut_arr, image):
-    #create_dir('./test')
-    #img_num = 1
+    create_dir('./test')
+    img_num = 1
     print(cut_arr)
-    #for cut in cut_arr:
-        #new_img = image.crop(cut)
-        #new_img.save('./test' + '/result' + str(img_num) + '.png')
-        #img_num += 1
+    for cut in cut_arr:
+        new_img = image.crop(cut)
+        # TODO ask for user input in where to save and name
+        new_img.save('./test' + '/result' + str(img_num) + '.png')
+        img_num += 1
 
 def create_tuple(left_arr, up_arr, right_arr, low_arr):
     cut_arr = []
-    # TODO use the last numbers if there are not enough to fill the rest of the tuples
-    # ie left = [1, 2] up = [1,2,3] 
-    # tuple = [1, 1], [2, 2], [2, 3]
-    for x in range(len(left_arr)): 
-        cut_tuple = (left_arr[x], up_arr[x], 
-                    right_arr[x], low_arr[x])
-        cut_arr.append(cut_tuple)
-        return cut_arr
-    else:
-        pass # TODO raise exception and print error
+    vertical = 0
+    while vertical < len(up_arr):
+        for x in range(len(left_arr)):
+            cut_tuple = (left_arr[x], up_arr[vertical], 
+                        right_arr[x], low_arr[vertical])
+            cut_arr.append(cut_tuple)
+        vertical += 1
+    return cut_arr
         
 def transparent(image, data): 
     width, height = image.size
@@ -44,24 +43,22 @@ def transparent(image, data):
             if i <= width - 1:
                 if data[i + (width * row_num)] is not 0:
                     row_num += 1
-                    #print("Row num is " + str(row_num))
                     i = 0
                     row_detect = True
                 elif i == width - 1 and row_detect == False:
                     i = 0
                     upper += 1
                     row_num += 1
-                    #print("Row num is " + str(row_num))
                 elif i == width - 1 and row_detect == True:
                     lower = row_num
 
                     low_arr.append(lower)
                     up_arr.append(upper)
 
-                    upper = row_num # Start new search here instead of beginning
+                    # Start new search here instead of the beginning
+                    upper = row_num 
 
                     row_num += 1
-                    #print("Row num is " + str(row_num))
                     row_detect = False
                     i = 0
                     continue
@@ -72,13 +69,11 @@ def transparent(image, data):
             if j <= height - 1:        
                 if data[col_num + (j * width)] is not 0: 
                     col_num += 1
-                    #print("Col num is " + str(col_num))
                     j = 0
                     col_detect = True
                 elif j == height - 1 and col_detect == False:
                     j = 0
                     col_num += 1
-                    #print("Col num is " + str(col_num))
                     left += 1
                 elif j == height - 1 and col_detect == True:
                     right = col_num    
@@ -89,7 +84,6 @@ def transparent(image, data):
                     left = col_num
              
                     col_num += 1
-                    #print("Col num is " + str(col_num))
                     col_detect = False
                     j = 0
                     continue
@@ -100,6 +94,7 @@ def transparent(image, data):
                            right_arr, low_arr)
         crop(arr, image)
 
+# TODO ask for user input for image
 image = Image.open('./test-images/test.png')
 pixels = image.convert('RGBA')
 data = list(pixels.getdata(3)) # Only gets Alpha Channels
