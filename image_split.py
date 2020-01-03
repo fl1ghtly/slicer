9aaab7d7c45497dc4dfb2f0c66cc8e0513273139
James Nguyen <59295993+fl1ghtly@users.noreply.github.com>
2020-01-02 21:32:30 -0800

    Cleaned up in order to fit PEP8 style guide

--- image_split.py
+++ image_split.py
@@ -5,6 +5,7 @@ left_arr, up_arr, right_arr, low_arr = [], [], [], []
 # Set default format
 img_format = '.png'
 
+
 def create_dir(dir_name):
     print('Creating file directory...')
     if not os.path.exists(dir_name):
@@ -12,6 +13,7 @@ def create_dir(dir_name):
             os.makedirs(dir_name)
         except OSError:
             print('Error creating ' + dir_name)
+
 
 def cut(cut_arr, image, img_format):
     print('Cropping...')
@@ -34,15 +36,16 @@ def cut(cut_arr, image, img_format):
 
         # Checks for blank cuts
         if not ImageOps.invert(new_img).getbbox():
-           pass 
+            pass
         else:
-            new_img.save(path 
-                         + '/' 
+            new_img.save(path
+                         + '/'
                          + os.path.splitext(os.path.split(image.filename)[1])[0]
-                         + str(img_num) 
+                         + str(img_num)
                          + img_format)
             img_num += 1
     print('Finished!')
+
 
 def create_tuple(left_arr, up_arr, right_arr, low_arr):
     cut_arr = []
@@ -50,77 +53,79 @@ def create_tuple(left_arr, up_arr, right_arr, low_arr):
     # Assigns each vertical pair with a horizontal pair 
     while vertical < len(up_arr):
         for x in range(len(left_arr)):
-            cut_tuple = (left_arr[x], up_arr[vertical], 
-                        right_arr[x], low_arr[vertical])
+            cut_tuple = (left_arr[x], up_arr[vertical],
+                         right_arr[x], low_arr[vertical])
             cut_arr.append(cut_tuple)
         vertical += 1
     return cut_arr
-        
-def transparent(image, data): 
+
+
+def transparent(image, data):
     print('Processing...')
     width, height = image.size
     row_num = col_num = i = j = 0
     left = upper = right = lower = int()
     # Used incase there are more than 1 pix gap for upper bound
-    row_detect = col_detect = end_search = False 
+    row_detect = col_detect = end_search = False
 
     while col_num <= width - 1 and row_num <= height - 1:
         # (left, *upper, right, *lower)      
-            if i <= width - 1:
-                if data[i + (width * row_num)] is not 0:
-                    row_num += 1
-                    i = 0
-                    row_detect = True
-                elif i == width - 1 and row_detect == False:
-                    i = 0
-                    upper += 1
-                    row_num += 1
-                elif i == width - 1 and row_detect == True:
-                    lower = row_num
+        if i <= width - 1:
+            if data[i + (width * row_num)] is not 0:
+                row_num += 1
+                i = 0
+                row_detect = True
+            elif i == width - 1 and not row_detect:
+                i = 0
+                upper += 1
+                row_num += 1
+            elif i == width - 1 and row_detect:
+                lower = row_num
 
-                    low_arr.append(lower)
-                    up_arr.append(upper)
+                low_arr.append(lower)
+                up_arr.append(upper)
 
-                    # Start new search here instead of the beginning
-                    upper = row_num 
+                # Start new search here instead of the beginning
+                upper = row_num
 
-                    row_num += 1
-                    row_detect = False
-                    i = 0
-                    continue
-                else:
-                    i += 1
+                row_num += 1
+                row_detect = False
+                i = 0
+                continue
+            else:
+                i += 1
 
         # (*left, upper, *right, lower)
-            if j <= height - 1:        
-                if data[col_num + (j * width)] is not 0: 
-                    col_num += 1
-                    j = 0
-                    col_detect = True
-                elif j == height - 1 and col_detect == False:
-                    j = 0
-                    col_num += 1
-                    left += 1
-                elif j == height - 1 and col_detect == True:
-                    right = col_num    
+        if j <= height - 1:
+            if data[col_num + (j * width)] is not 0:
+                col_num += 1
+                j = 0
+                col_detect = True
+            elif j == height - 1 and not col_detect:
+                j = 0
+                col_num += 1
+                left += 1
+            elif j == height - 1 and col_detect:
+                right = col_num
 
-                    left_arr.append(left)
-                    right_arr.append(right)
+                left_arr.append(left)
+                right_arr.append(right)
 
-                    left = col_num
-             
-                    col_num += 1
-                    col_detect = False
-                    j = 0
-                    continue
-                else:
-                    j += 1
+                left = col_num
+
+                col_num += 1
+                col_detect = False
+                j = 0
+                continue
+            else:
+                j += 1
     else:
-        arr = create_tuple(left_arr, up_arr, 
+        arr = create_tuple(left_arr, up_arr,
                            right_arr, low_arr)
         cut(arr, image, img_format)
 
-# Checks for valid user input and arguements
+
+# Checks for valid user input and arguments
 try:
     if len(sys.argv) > 1:
         try:
@@ -135,10 +140,10 @@ try:
                 elif sys.argv[2].find('.') == 0:
                     img_format = sys.argv[2]
                 else:
-                    raise RuntimeError 
+                    raise RuntimeError
             except RuntimeError:
                 sys.exit('Invalid Image Format')
-    else: 
+    else:
         raise RuntimeError
 except RuntimeError:
     sys.exit('No commands inputted')
@@ -147,5 +152,5 @@ except FileNotFoundError:
 
 
 pixels = image.convert('RGBA')
-data = list(pixels.getdata(3)) # Only gets Alpha Channels
+data = list(pixels.getdata(3))  # Only gets Alpha Channels
 transparent(image, data)
