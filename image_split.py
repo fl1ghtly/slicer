e6399b4fff2e97099439f36720cf46403a7eaf83
CrazyConga TheGreat <hamburger95764@gmail.com>
2021-02-12 21:28:58 -0800

    added bounds function
    finds the upper and lower bounds of a given shape

--- image_split.py
+++ image_split.py
@@ -12,8 +12,26 @@ def create_dir(dir_name):
         except OSError:
             print('Error creating ' + dir_name)
 
-def find_points(coordinate_array, direction):
-    print()
+# Finds the upper and lower bounds of the shape
+def find_bounds(coordinate_array, direction, width, height):
+    marked_points = []
+    bounds = []
+
+    if direction is "Y":
+        for y in range(height):
+            for x in range(width):
+                if coordinate_array.get((x, y)) > 0:
+                    marked_points.append(y)
+    else:
+        for x in range(width):
+            for y in range(height):
+                if coordinate_array.get((x, y)) > 0:
+                    marked_points.append(x)
+
+    bounds.append(min(marked_points))
+    bounds.append(max(marked_points))
+
+    return bounds
 
 
 # From the given point, create a coordinate pair value
@@ -69,3 +87,5 @@ pixels = image.convert('RGBA')
 width, height = image.size
 data = list(pixels.getdata(3))  # Only gets Alpha Channels
 coord_arr = create_coordinates(data, width, height)
+y_bounds = find_bounds(coord_arr, "Y", width, height)
+x_bounds = find_bounds(coord_arr, "X", width, height)
